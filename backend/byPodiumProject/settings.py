3c5ada8a2f1242448dcd95869ac6c0277862d7ec
gabriel <ramos.gabrielsilva23@gmail.com>
2025-07-08 17:38:32 -0300

    chore: reestructing folders

--- backend/byPodiumProject/settings.py
+++ backend/byPodiumProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*-jjgpu8)wzaznzc4jyi^1*0y97wen88cbwyz=%68plotz%tkt'
+SECRET_KEY = 'django-insecure-2_+11x(i^=0+%(qwbl^_ae3#w18*8lj)d17kofv^0q$wn@*8qq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
